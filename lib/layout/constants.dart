d6e3401d9825a1d2d60195b10261bdfe030aefc2
signup <mansterghj@gmail.com>
2022-05-20 14:49:03 +0200

    add signup screen

---
+++ lib/layout/constants.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+Widget defaultFormField({
+  required TextEditingController controller,
+  required TextInputType type,
+  ValueChanged? onSubmit,
+  ValueChanged? onChange,
+  GestureTapCallback? onTap,
+  bool isPassword = false,
+  required FormFieldValidator validate,
+  required String label,
+  required IconData prefix,
+  IconData? suffix,
+  VoidCallback? suffixPressed,
+  bool isClickable = true,
+}) =>
+    TextFormField(
+      controller: controller,
+      keyboardType: type,
+      obscureText: isPassword,
+      enabled: isClickable,
+      onFieldSubmitted: onSubmit,
+      onChanged: onChange,
+      onTap: onTap,
+      validator: validate,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(
+          prefix,
+        ),
+        suffixIcon: suffix != null
+            ? IconButton(
+          onPressed: suffixPressed,
+          icon: Icon(
+            suffix,
+          ),
+        )
+            : null,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15)
+        ),
+      ),
+    );
+
+Widget defaultButton({
+  double width = double.infinity,
+  Color background = Colors.blue,
+  bool isUpperCase = true,
+  double radius = 3.0,
+  required VoidCallback function,
+  required String text,
+}) =>
+    Container(
+      width: width,
+      height: 50.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          15,
+        ),
+        color: background.withOpacity(.6),
+      ),
+      child: MaterialButton(
+        onPressed: function,
+        child: Text(
+          isUpperCase ? text.toUpperCase() : text,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+Widget defaultTextButton({
+  required VoidCallback function,
+  required String text,
+}) =>
+    TextButton(
+      onPressed: function,
+      child: Text(
+        text.toUpperCase(),
+      ),
+    );
